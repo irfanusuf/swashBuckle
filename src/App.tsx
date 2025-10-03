bc9c30be9f942241c53d2250e67214b92de4b553
Irfan Yousuf <irfanusuf33@gmail.com>
2025-10-03 15:50:08 +0530

    fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,14 @@
-// import HeroSection from "./pages/HeroSection"
+import HeroSection from "./pages/HeroSection"
 
 
 
 
 function App() {
-
-
   return (
     <>
       <h1> Hello World this is react RTK in typescript </h1>
-
-
-
       <h3>  This is the Child Component </h3>
-
-        {/* <HeroSection username="irfan"/> */}
+      <HeroSection username="tehleem"/>
     </>
   )
 }
--- src/pages/HeroSection.tsx
+++ src/pages/HeroSection.tsx
@@ -1,60 +1,96 @@
 
+// type heroSectionProps =  {
 
+import React, { useEffect, useState } from "react"
 
-// // type heroSectionProps =  {
+//     username : string
 
-// import { useState } from "react"
-
-// //     username : string
-
-// // }
-
-
-// interface heroSectionInterface  {
-//     username : string,
-//     age?: number
 // }
 
 
-// const HeroSection = ({username } : heroSectionInterface) => {
+interface heroSectionInterface {
+    username: string,
+    age?: number
+}
+
+
+const HeroSection = ({ username }: heroSectionInterface) => {
+
+
+    // const [admin, setIsAdmin] = useState<boolean>(false)
+
+    const [user, setUser] = useState<User>()
+
+    // const [post, setPost] = useState<string>("")
+
+
+    // const [count, setCount] = useState<number>(0);
+
+
+    // const [status, setStatus] = useState<'idle' | 'loading' | 'error'>('idle');
+
+
+    // // setIsAdmin(3) // u cant pass number into boolean 
+
+    // //  setCount("sdnjfbvsdjh")   // u cant pass string in  number 
+
+
+    // setCount((count) => count + 1)    // correct
+
+    // setStatus("loading")
+
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        console.log(e)
+    }
+
+
+    const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+
+
+        console.log(e)
+    }
+
+    interface User {
+
+        username: string,
+        email: string,
+        profilePic: string,
+        isAdmin: boolean,
+        Role: string
+
+    }
 
 
 
-//     const [admin , setIsAdmin] = useState<boolean>(false)
+    const fetchData = async (url: string): Promise<User> => {
+        const res = await fetch(url);
+        if (res.status === 200) {
+            return await res.json() as Promise<User>
+        } else {
+            throw new Error("Network Error !")
+        }
+    }
 
-//     const [post , setPost] = useState<string>("")
-
-
-//     const [count, setCount] = useState<number>(0);
-
-
-//     const [status, setStatus] = useState<'idle' | 'loading' | 'error'>('idle');
-
-
-//     // setIsAdmin(3)  u cant pass number into boolean 
-
-//     // setCount("sdnjfbvsdjh")   // u cant pass string in  number 
-
-
-//     // setCount((count) => count + 1)    // correct
-
-//     setStatus("loading")
+    useEffect(
+        () => {
+            (async () => {
+                const data = await fetchData("sjhfgbsjhfgshdjgf")
+                setUser(data)
+            })()
+        }
+    )
 
 
+    return (
+        <div>
+            <h2> This is a  HeroSection </h2>
+            <h3> Welcome {username}</h3>
+            <input placeholder="Enter Something" onChange={(e) => { handleChange(e) }} />
+            <button onClick={(e) => { handleClick(e) }}> Click Here </button>
+            <p> {user?.username ?? "john doe"}</p>
+        </div>
+    )
+}
 
-//     return (
-//         <div>
-
-
-
-
-//           <h2> This is a  HeroSection </h2> 
-//           <h3> Welcome {username}</h3>
-
-
-
-//         </div>
-//     )
-// }
-
-// export default HeroSection
+export default HeroSection
